f0bf0711af23421853a9595b12bccd355016d411
sharifa-assi <sharifaalassi2001@gmail.com>
2022-02-13 17:52:09 +0200

    user requests

--- index.js
+++ index.js
@@ -4,14 +4,23 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL,()=>{
-    console.log("Connected to MongoDB")
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Connected to MongoDB");
 });
 
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
-app.listen(8800,()=>{
-    console.log("Backend server is running!")
-})
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
